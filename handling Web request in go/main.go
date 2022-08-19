b1fac292dac59b40526f2e840423a4ac68f94021
Dipraj Daripa <diprajdaripabnk@gmail.com>
2022-08-19 06:12:36 +0530

    handling web responce

--- handling Web request in go/main.go
+++ handling Web request in go/main.go
@@ -11,7 +11,7 @@ const url = "https://lco.dev" // any url give
 func main() {
 	fmt.Println("LCO webSite web request")
 
-	response, err := http.Get(url)
+	response, err := http.Get(url) // this is type of a pointer return meanse original data of this webSite ... you can manipulate it and changes occour in original data in website
 	checkErrorNil(err)
 
 	fmt.Printf("Response is of type: %T\n", response) // response was a pointer ... so its return [*http.Response]
